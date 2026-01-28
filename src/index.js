9b6131d754ea3afda3d9d00d5d4ad5bf489cdbbe
LeoPavlovski <leopavlovski01@gmail.com>
2026-01-28 15:02:50 +0100

    feat: Using useEffect to fetch data after the render of the App function on mount.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
-// import './index.css';
+import "./index.css";
 // import App from './App';
+import Appv2 from "./Appv1";
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -17,11 +18,12 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    {/*<StarRating*/}
+    {/*  maxRating={5}*/}
+    {/*  messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}*/}
+    {/*/>*/}
+    {/*<StarRating size={24} color="red" className="test" defaultRating={3} />*/}
+    {/*<Test />*/}
+    <Appv2></Appv2>
   </React.StrictMode>,
 );
